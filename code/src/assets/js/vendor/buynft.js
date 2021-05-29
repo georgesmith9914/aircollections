79b3a1b69e16de714c48ef4368a6b272fc8cac23
Chain Future <chain.future.forward@gmail.com>
2021-05-29 09:11:21 +0200

    Initial files created

---
+++ code/src/assets/js/vendor/buynft.js
@@ -0,0 +1,14 @@
+function buyNFT(){
+    console.log("Entered in buyNFT function..");
+
+    
+    //Open Metamask and transfer funds from buyer to creator
+    var web3  = new Web3(web3.currentProvider);
+    // Acccounts always exposed
+    web3.eth.sendTransaction({/* ... */});
+    //mint NFT
+
+
+    //transfer NFT to buyer
+
+}
