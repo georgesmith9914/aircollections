6937eea2b6ec18cda3801151b06067b106347c2e
Chain Future <chain.future.forward@gmail.com>
2021-05-30 08:56:49 +0200

    Updated for livepeer

--- code/src/assets/js/vendor/hlsplayer.js
+++ code/src/assets/js/vendor/hlsplayer.js
@@ -2,8 +2,8 @@ var video = document.getElementById('video');
 var counter = 0;
 //var streamId = '1220b0336cf5c3c34c0a05e5f8d9a6fb8874a091ff47a9546cf748d8e93827b0050101123a74ec79ab1edbafebf2d55a51b33cc86ea567bb71e04f76030c432c6ec5'
 //var source = 'http://localhost:8935/stream/' + streamId + '.m3u8'
-//var source = 'https://cdn.livepeer.com/hls/d4dfdt7k4za7dsyb/index.m3u8'
-var source = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+var source = 'https://cdn.livepeer.com/hls/d4dfdt7k4za7dsyb/index.m3u8'
+//var source = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
 if(Hls.isSupported()) {
   var hls = new Hls();
   hls.loadSource(source);
